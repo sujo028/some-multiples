2150bd9c65f9ba841bf5a3656689981cc0b77780
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-10-24 20:49:14 +0530

    Sum.c
    Sum of multipies

--- sum.py
+++ sum.py
@@ -1,5 +1,11 @@
-sum=0
-for i in range(1,1000)
- if(i%3==0 or i%5==0)
-  sum=sum+i
-print sum
+#include<studio.h>
+void main()
+{
+  int sum=0;
+  for(int i=0;i<1000;i+1)
+  {
+    if(i%3==0 or i%5==0)
+        sum=sum+i;
+  }
+  printf("Sum of multiplies=%d",i);
+}
