35c3782104ee31ba4ef72571ddac26abced96ed0
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-10-24 20:39:23 +0530

    Sum.c
    Sum of multiples ;

--- sum.py
+++ sum.py
@@ -1,5 +1,14 @@
-sum=0
-for i in range(1,1000)
- if(i%3==0 or i%5==0)
-  sum=sum+i
-print sum
+#include<studio.h>
+void main()
+{
+  int sum,i;
+  for(i=0;i<1000;i++)
+   {
+      if(i%3==0 && i%5==0)
+      {
+        sum+=i;
+      }
+   
+   }
+ printf("Sum of multiplies =%d",i);
+}
