cb94fb5b23bfd933cdc76ab8b80bad93d993d101
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-11-03 22:39:38 +0530

    Sum.cpp

--- sum.py
+++ sum.py
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include<iostream.h>
 void main()
 { int sum=0,i;
   for(i=0;i<1000;i+1){
    if(i%3==0 or i%5==0)
       sum=sum+i;}
-   printf("Sum=%d",sum);
+  cout<<"Sum=",sum;
 }
